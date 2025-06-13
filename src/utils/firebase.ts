4eadb4620832677280e422634f933233579a9239
Kethaka Janadithya Ranasinghe <xiojanadithya@gmail.com>
2025-06-13 13:36:52 +0530

    Update firebase config to use env vars

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,14 +4,23 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAXdhpMtHXDX1YvJkI73o5QVr3mk-CgME4",
+//     authDomain: "freelancer-portfolio-75abd.firebaseapp.com",
+//     projectId: "freelancer-portfolio-75abd",
+//     storageBucket: "freelancer-portfolio-75abd.firebasestorage.app",
+//     messagingSenderId: "274985156871",
+//     appId: "1:274985156871:web:2926758889697da7ec85a2",
+//     measurementId: "G-D8HDLV0C5J"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAXdhpMtHXDX1YvJkI73o5QVr3mk-CgME4",
-    authDomain: "freelancer-portfolio-75abd.firebaseapp.com",
-    projectId: "freelancer-portfolio-75abd",
-    storageBucket: "freelancer-portfolio-75abd.firebasestorage.app",
-    messagingSenderId: "274985156871",
-    appId: "1:274985156871:web:2926758889697da7ec85a2",
-    measurementId: "G-D8HDLV0C5J"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
